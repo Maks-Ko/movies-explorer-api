7270e7d630b52087c0fff7cb2f0ffdd4c5b2089f
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2021-12-23 20:56:59 +0500

    feat: подключить базу данных

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/dipfilmsdb', {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`); // eslint-disable-line no-console
